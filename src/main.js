a1398017da761ff40ce67736804c271e825d608a
Tommy <tommyopeters@gmail.com>
2021-11-03 07:04:51 +0100

    Boiler Plate

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// focus-on-input directive
+Vue.directive("focus", {
+  // When the bound element is inserted into the DOM...
+  inserted: function (el) {
+    // Focus the element
+    el.focus();
+  },
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
